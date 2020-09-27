d1609b13e14f75396fe757619d8feca8604fb77c
Gabriel Farias <70359452+GabrielFarias9989@users.noreply.github.com>
2020-09-27 10:52:46 -0300

    Update script.js

--- Aula12/ex14/script.js
+++ Aula12/ex14/script.js
@@ -7,15 +7,15 @@ function carregar() {
 	msg.innerHTML = `Agora são ${hora}horas.`
 	if (hora >= 0 && hora < 12) {
 		// BOM DIA!
-		img.src = 'fotomanha.png'
+		img.src = 'img/fotomanha.png'
 		document.body.style.background = '#e2cd9f'
 	} else if (hora >= 12 && hora < 18) {
 		// BOA TARDE!
-		img.src = 'fototarde.png'
+		img.src = 'img/fototarde.png'
 		document.body.style.background = '#b9846f'
 	} else {
 		// BOA NOITE!
-		img.src = 'fotonoite.png'
+		img.src = 'img/fotonoite.png'
 		document.body.style.background = '#515154'
 	}
 }
